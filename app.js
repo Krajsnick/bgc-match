16b544078ed6bd5b5609241153054d96c900c3ce
Robert Ahlberg <rob.ahlberg@gmail.com>
2015-12-07 18:53:11 +0100

    Boilerplate

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+logger = require('morgan'),
+request = require('request'),
+cheerio = require('cheerio'),
+app = express();
+
+// Logger config
+if (process.env.NODE_ENV == 'production')
+  app.use(logger('short'));
+else
+  app.use(logger('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var listener = app.listen(3000, function() {
+  console.log("bgc-match server started on port " + listener.address().port);
+});
+
+console.log("BAJS");
